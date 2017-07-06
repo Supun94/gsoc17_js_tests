c749b1f3ec6a8905790230e181b3f623e7a749a0
Supun94 <supunpramoda@gmail.com>
2017-07-06 22:33:29 +0530

    Adding test coverage for frontediting script

---
+++ tests/javascript/frontediting/spec-setup.js
@@ -0,0 +1,15 @@
+/**
+ * @package     Joomla.Tests
+ * @subpackage  JavaScript Tests
+ *
+ * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ *
+ * @since       3.6.3
+ * @version     1.0.0
+ */
+
+define(['jquery', 'text!testsRoot/frontediting/fixtures/fixture.html', 'libs/core', 'libs/frontediting'], function ($, fixture) {
+	$('body').append(fixture);
+	Joomla.fediting();
+});
