d4749f0dae3e077c916e03bd75d004ed135a0010
Supun94 <supunpramoda@gmail.com>
2017-09-04 00:21:20 +0530

    Updating the test suite

--- tests/javascript/helpsite/spec-setup.js
+++ tests/javascript/helpsite/spec-setup.js
@@ -9,6 +9,29 @@
  * @version     1.0.0
  */
 
-define(['jquery', 'text!testsRoot/helpsite/fixtures/fixture.html', 'libs/core'], function ($, fixture) {
+define(['jquery', 'text!testsRoot/helpsite/fixtures/fixture.html', 'libs/helpsite', 'libs/core'], function ($, fixture) {
 	$('body').append(fixture);
+	
+	helpSiteResponses = {
+		success: {
+			status: 200,
+			statusText: 'HTTP/1.1 200 OK',
+			responseText: '{"messages": {"message": "text"}}'
+		},
+		successInvalid: {
+			status: 200,
+			statusText: 'HTTP/1.1 200 OK',
+			responseText: '{"messages": "text"}'
+		},
+		fail: {
+			status: 404,
+			statusText: 'HTTP/1.1 404 Not Found',
+			responseText: 'Error'
+		}
+	};
+	
+	select_id   = 'rel';
+	showDefault = 'showDefault';
+	
+	$('#helpsite').click(resetHelpSiteList);
 });
--- tests/javascript/helpsite/spec.js
+++ tests/javascript/helpsite/spec.js
@@ -10,5 +10,43 @@
  */
 
 define(['jquery', 'testsRoot/helpsite/spec-setup', 'jasmineJquery'], function ($) {
-
+	
+	describe('Helpsite', function () {
+		beforeAll(function() {
+			jasmine.Ajax.install();
+			
+			renderFn = Joomla.renderMessages;
+			removeFn = Joomla.removeMessages;
+			ajxerrFn = Joomla.ajaxErrorsMessages;
+			scrollFn = window.scrollTo;
+			
+			Joomla.renderMessages = jasmine.createSpy('renderMessages');
+			Joomla.removeMessages = jasmine.createSpy('removeMessages');
+			Joomla.ajaxErrorsMessages = jasmine.createSpy('ajaxErrorsMessages');
+			window.scrollTo = jasmine.createSpy('scrollTo');
+			
+			$('#helpsite').click();
+		});
+		
+		afterAll(function () {
+			jasmine.Ajax.uninstall();
+			
+			Joomla.renderMessages = renderFn;
+			Joomla.removeMessages = removeFn;
+			Joomla.ajaxErrorsMessages = ajxerrFn;
+			window.scrollTo = scrollFn;
+		});
+		
+		describe("on success with typeof item.value !== '' ", function() {
+			beforeAll(function() {
+				request = jasmine.Ajax.requests.mostRecent();
+				request.respondWith(helpSiteResponses.successInvalid);
+			});
+			
+			it("should call Joomla.resetHelpSiteList({})", function() {
+				expect(resetHelpSiteList).not.toHaveBeenCalledWith({});
+			});
+		});
+		
+	});
 });
