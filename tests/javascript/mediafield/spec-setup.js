13a3546711a58c53db3d92f736babc30d2eb24fa
Supun94 <supunpramoda@gmail.com>
2017-09-05 20:04:52 +0530

    Initial commit

---
+++ tests/javascript/mediafield/spec-setup.js
@@ -0,0 +1,12 @@
+/**
+ * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @package     Joomla
+ * @subpackage  JavaScript Tests
+ * @since       3.7.0
+ * @version     1.0.0
+ */
+
+define(['jquery', 'text!testsRoot/mediafield/fixtures/fixture.html', 'libs/fields/mediafield'], function ($, fixture) {
+	$('body').append(fixture);
+});
